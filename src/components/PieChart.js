a6c7e985bc97d30baa05b7e444952e5132035e7d
hussainarslan <l201266@lhr.nu.edu.pk>
2023-05-14 21:53:44 +0500

    pie chart added

---
+++ src/components/PieChart.js
@@ -0,0 +1,72 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { Pie } from 'react-chartjs-2'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+
+
+const PieChart = ({username}) => {
+    const [expenses, setExpenses] = useState(0);
+    const [goals, setGoals] = useState(0);
+    
+    // const [dataarr, setData] = useState([]);
+
+    const data = {
+        labels: ['Spent', 'Available'],
+        datasets: [
+            {
+                data: [expenses, goals],
+                backgroundColor: ['orangered', 'aqua']
+            }
+        ]
+    }
+
+    const options = {
+        
+    }   
+
+    ChartJS.register(
+        ArcElement,
+        Tooltip,
+        Legend
+    );
+
+
+    const getExpenses = async () => {
+        try {
+
+            const fetchData = await axios.post('http://localhost:5000/expenses', {"username": {username}});
+            let result = fetchData.data.map(({amount}) => amount).reduce((partialSum, a) => partialSum + a, 0);
+            setExpenses(result);
+            
+        } catch(err){
+            console.log(err);
+        }
+    }
+
+
+    const getGoals = async () => {
+        try {
+
+            const fetchData = await axios.post('http://localhost:5000/goals', {"username": {username}});
+            let result = fetchData.data.map(({amount}) => amount).reduce((partialSum, a) => partialSum + a, 0);
+            setGoals(result - expenses);
+            
+        } catch(err){
+            console.log(err);
+        }
+    }
+
+    const loadData = () => {
+        getExpenses();
+        getGoals();
+    }
+    
+    return (
+        <div onLoad={loadData()}>
+
+            <Pie data={data} options={options}></Pie>
+        </div>
+    )
+}
+
+export default PieChart
