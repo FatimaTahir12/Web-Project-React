bdaa223905684d8636bf80166ad63c380fd49ad8
Rakeen Zia <82565184+ROCKRAJA949@users.noreply.github.com>
2023-05-17 14:38:37 +0500

    Revert "trying the impossible"

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import Notification from './Notification'
-import { useState } from 'react';
+
 const Notifications = ({notifications}) => {
-  
-  const [data, setData] = useState(notifications.map(function({category, goal_amount, expense_amount}) {if(((goal_amount - expense_amount)/goal_amount) > 0.8)  return [category, ((goal_amount - expense_amount)/goal_amount)*100] }));
-     
-  
-  
   return (
     <div>
         <ul className="notification-list">
-        {data.map(function(notification){
+        {notifications.map(function(notification){
           return <li><Notification data= {notification}/></li>
         })}
         </ul>
