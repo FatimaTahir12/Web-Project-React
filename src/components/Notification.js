de1ba44376ee86cb093850747eb7ea6ad83d317f
ROCKRAJA949 <mohammadrockraja949@gmail.com>
2023-05-17 22:42:24 +0500

    notifications done

--- src/components/Notification.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Notification = ({notification}) => {
-  return (
-        <p>You have used up {notification}% of your limit in category {notification}</p>
-  )
-}
-
-export default Notification
