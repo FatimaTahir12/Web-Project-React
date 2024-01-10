ca4f019cdafbecafff4100f4d3a55d303ee76e3e
FatimaTahir12 <l201275@lhr.nu.edu.pk>
2024-01-11 01:22:12 +0500

    new changes

--- src/server/index.js
+++ src/server/index.js
@@ -14,9 +14,8 @@ const { MONGO_URI } = process.env;
 app.use(express.json());
 
 
-mongoose.connect(
-    "mongodb://projectadmin:admin@ac-mdbk4pf-shard-00-00.uqshvwe.mongodb.net:27017,ac-mdbk4pf-shard-00-01.uqshvwe.mongodb.net:27017,ac-mdbk4pf-shard-00-02.uqshvwe.mongodb.net:27017/project?ssl=true&replicaSet=atlas-149q8f-shard-0&authSource=admin&retryWrites=true&w=majority"
-);
+mongoose.connect(""
+   );
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
